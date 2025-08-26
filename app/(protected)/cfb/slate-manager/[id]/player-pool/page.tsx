2cdb8250b6b0226367b2266c4e9537d4b6439578
Reggie Evans <REvans911@gmail.com>
2025-08-26 14:23:25 -0500

    remove console logs

--- app/(protected)/cfb/slate-manager/[id]/player-pool/page.tsx
+++ app/(protected)/cfb/slate-manager/[id]/player-pool/page.tsx
@@ -63,7 +63,6 @@ export default function PlayerPoolPage() {
   }
 
   const handleSubmitTarget = async (values: TargetPlayerFormValues) => {
-    console.log('values', values)
     try {
       const payload = {
         id: existingTarget?.id,
