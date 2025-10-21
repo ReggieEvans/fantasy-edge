c06d5392e0800a9dd28cd593b5095336ae0cc027
Reggie Evans <REvans911@gmail.com>
2025-10-20 20:15:16 -0500

    dashboard updates

--- features/dashboard/ui/DashboardLinks.tsx
+++ features/dashboard/ui/DashboardLinks.tsx
@@ -9,45 +9,51 @@ export default function DashboardLinks() {
       <div className="flex gap-16 flex-wrap p-4">
         <div className="flex flex-col gap-2">
           <h3 className="text-sm font-bold text-foreground">One Week Season</h3>
-          <Link href="https://www.espn.com" target="_blank">
+          <Link href="https://oneweekseason.com/projections-dk/" target="_blank">
             <span className="text-sm text-muted">NFL Projections</span>
           </Link>
-          <Link href="https://www.espn.com" target="_blank">
+          <Link href="https://oneweekseason.com/ownership-home/" target="_blank">
             <span className="text-sm text-muted">NFL Ownership</span>
           </Link>
-          <Link href="https://www.espn.com" target="_blank">
+          <Link href="https://oneweekseason.com/the-scroll/" target="_blank">
             <span className="text-sm text-muted">NFL Scroll</span>
           </Link>
         </div>
         <div className="flex flex-col gap-2">
           <h3 className="text-sm font-bold text-foreground">CFF Site</h3>
-          <Link href="https://www.espn.com" target="_blank">
+          <Link
+            href="https://thecollegefantasyfootballsite.com/2024-weekly-projections-full-fbs-1-0-ppr/"
+            target="_blank"
+          >
             <span className="text-sm text-muted">CFB Projections</span>
           </Link>
-          <Link href="https://www.espn.com" target="_blank">
+          <Link
+            href="https://thecollegefantasyfootballsite.com/2025-weekly-dfs-portal/"
+            target="_blank"
+          >
             <span className="text-sm text-muted">CFB Analysis</span>
           </Link>
         </div>
         <div className="flex flex-col gap-2">
           <h3 className="text-sm font-bold text-foreground">Resources</h3>
-          <Link href="https://www.espn.com" target="_blank">
+          <Link href="https://premium.pff.com/nfl/games/2025/" target="_blank">
             <span className="text-sm text-muted">Pro Football Focus</span>
           </Link>
-          <Link href="https://www.espn.com" target="_blank">
+          <Link href="https://www.teamrankings.com/nfl/stat/passing-play-pct" target="_blank">
             <span className="text-sm text-muted">Team Rankings</span>
           </Link>
-          <Link href="https://www.espn.com" target="_blank">
+          <Link href="https://www.rotowire.com/football/news.php" target="_blank">
             <span className="text-sm text-muted">Rotowire News</span>
           </Link>
         </div>
         <div className="flex flex-col gap-2">
           <h3 className="text-sm font-bold text-foreground">Admin</h3>
-          <Link href="https://www.espn.com" target="_blank">
-            <span className="text-sm text-muted">FE Data Sync</span>
-          </Link>
-          <Link href="https://www.espn.com" target="_blank">
-            <span className="text-sm text-muted">Lineup Exporter</span>
-          </Link>
+          <span className="text-sm text-muted opacity-50 hover:cursor-not-allowed">
+            FE Data Sync
+          </span>
+          <span className="text-sm text-muted opacity-50 hover:cursor-not-allowed">
+            Lineup Exporter
+          </span>
         </div>
       </div>
     </div>
